f5c569f0d12f533b1e0930577d7dce3acd5e513e
fonztara <fonztara@gmail.com>
2025-05-31 18:28:58 +0200

    Header fixed on top

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Welcome to my portfolio website! 🚀");
+const header = document.getElementById("header");
+const hero = document.getElementById("hero");
+
+function checkIfOffScreen() {
+  const rect = hero.getBoundingClientRect();
+  const isOffScreen =
+    rect.bottom < 0 ||
+    rect.top > window.innerHeight ||
+    rect.right < 0 ||
+    rect.left > window.innerWidth;
+
+  if (isOffScreen) {
+    header.classList.remove("hidden");
+  } else {
+    if (!(header.classList.contains("hidden"))) {
+      header.classList.add("hidden");
+    }
+  }
+}
+
+let ticking = false;
+
+window.addEventListener("scroll", () => {
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      checkIfOffScreen();
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
