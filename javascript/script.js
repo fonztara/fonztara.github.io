48ba681d1436e50c03ec12d34808a2730903be43
fonztara <fonztara@gmail.com>
2025-06-08 16:45:41 +0200

    Project Detail

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,37 @@
-const links = document.querySelector(".header-right ol")
+const links = document.querySelector(".header-right ol");
 
 function showMenu() {
-    if (links.classList.contains("hidden")) {
-        links.classList.remove("hidden")
-    } else {
-        closeMenu()
-    }
+  if (links.classList.contains("hidden")) {
+    links.classList.remove("hidden");
+  } else {
+    closeMenu();
+  }
 }
 
 function closeMenu() {
-    links.classList.add("hidden")
+  links.classList.add("hidden");
+}
+
+const detail = document.querySelector(".project-detail");
+const detailContent = detail.querySelector(".project-detail-content")
+const body = document.body
+
+function showDetail(projectTitle) {
+  if (!detail.classList.contains("showing")) {
+    detailContent.querySelector("h2").innerHTML = projectTitle;
+
+    const description = document.querySelector(".project-description." + projectTitle)
+
+    detailContent.querySelector("p").innerHTML = description.innerHTML
+
+    body.classList.add("no-scroll")
+    detail.classList.add("showing");
+  }
+}
+
+function hideDetail() {
+    if (detail.classList.contains("showing")) {
+        body.classList.remove("no-scroll")
+        detail.classList.remove("showing");
+      }
 }
