f8a3f9d21a16e3569f3d512cb4c102214a3fbda0
fonztara <fonztara@gmail.com>
2025-06-09 02:44:39 +0200

    Experience Section

--- javascript/script.js
+++ javascript/script.js
@@ -1,37 +1,57 @@
+const body = document.body;
 const links = document.querySelector(".header-right ol");
+const detail = document.querySelector(".project-detail");
+const detailContent = detail.querySelector(".project-detail-content");
+const headerButton = document.querySelector(".header-right button");
 
-function showMenu() {
+function toggleMenu() {
+  hideDetail();
+  if (headerButton.classList.contains("isOpen")) {
+    headerButton.classList.remove("isOpen")
+  } else {
+    headerButton.classList.add("isOpen")
+  }
   if (links.classList.contains("hidden")) {
     links.classList.remove("hidden");
   } else {
-    closeMenu();
+    links.classList.add("hidden");
   }
 }
 
 function closeMenu() {
-  links.classList.add("hidden");
+  hideDetail();
+  if (headerButton.classList.contains("isOpen")) {
+    headerButton.classList.remove("isOpen")
+  } else {
+    headerButton.classList.add("isOpen")
+  }
+  if (!links.classList.contains("hidden")) {
+    links.classList.add("hidden");
+  }
 }
-
-const detail = document.querySelector(".project-detail");
-const detailContent = detail.querySelector(".project-detail-content")
-const body = document.body
 
 function showDetail(projectTitle) {
   if (!detail.classList.contains("showing")) {
     detailContent.querySelector("h2").innerHTML = projectTitle;
 
-    const description = document.querySelector(".project-description." + projectTitle)
+    const description = document.querySelector(
+      ".project-description." + projectTitle
+    );
 
-    detailContent.querySelector("p").innerHTML = description.innerHTML
+    detailContent.querySelector("p").innerHTML = description.innerHTML;
 
-    body.classList.add("no-scroll")
+    body.classList.add("no-scroll");
     detail.classList.add("showing");
   }
 }
 
 function hideDetail() {
-    if (detail.classList.contains("showing")) {
-        body.classList.remove("no-scroll")
-        detail.classList.remove("showing");
-      }
+  if (detail.classList.contains("showing")) {
+    body.classList.remove("no-scroll");
+    detail.classList.remove("showing");
+  }
+}
+
+function generateDetail(projectTitle) {
+  
 }
