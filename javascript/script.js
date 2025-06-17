b27888a757c5bb056acef446810be34f8b798bd7
fonztara <fonztara@gmail.com>
2025-06-17 19:49:15 +0200

    Improvements

--- javascript/script.js
+++ javascript/script.js
@@ -40,6 +40,17 @@ function showDetail(projectTitle) {
 
     detailContent.querySelector("p").innerHTML = description.innerHTML;
 
+    const img = document.createElement("img");
+    img.src = "../assets/photos/alfoSmall.png";
+    const img2 = document.createElement("img");
+    img2.src = "../assets/photos/alfoSmall.png";
+    const img3 = document.createElement("img");
+    img3.src = "../assets/photos/alfoSmall.png";
+
+    detailContent.querySelector(".gallery").appendChild(img);
+    detailContent.querySelector(".gallery").appendChild(img2);
+    detailContent.querySelector(".gallery").appendChild(img3);
+
     body.classList.add("no-scroll");
     detail.classList.add("showing");
   }
@@ -49,6 +60,8 @@ function hideDetail() {
   if (detail.classList.contains("showing")) {
     body.classList.remove("no-scroll");
     detail.classList.remove("showing");
+
+    detailContent.querySelector(".gallery").innerHTML = "";
   }
 }
 
